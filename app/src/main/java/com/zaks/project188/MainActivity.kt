59661d6f7d5c06168e5b8c603e2dca938c390d8a
danikzaks <zaksdanil1913@gmail.com>
2024-06-12 19:33:01 +0300

    git init and create rect. with rounder edges

---
+++ app/src/main/java/com/zaks/project188/MainActivity.kt
@@ -0,0 +1,88 @@
+package com.zaks.project188
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import com.zaks.project188.ui.theme.Project188Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            dashboard()
+        }
+    }
+
+    @Preview(showSystemUi = true)
+    @Composable
+    private fun dashboard() {
+        Column(
+            Modifier
+                .fillMaxHeight()
+                .fillMaxWidth()
+                .background(color = Color(android.graphics.Color.parseColor("#f8eeec"))),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            ConstraintLayout {
+                val (topImg, profile) = createRefs()
+                Box(Modifier
+                    .fillMaxWidth()
+                    .height(245.dp)
+                    .constrainAs(topImg) {
+                        top.linkTo(parent.top)
+                        start.linkTo(parent.start)
+                    }
+                    .background(
+                        brush = Brush.horizontalGradient(
+                            colors = listOf(
+                                Color(android.graphics.Color.parseColor("#EA6D35")),
+                                Color(android.graphics.Color.parseColor("#3b608c"))
+                            )
+                        ), shape = RoundedCornerShape(bottomEnd = 40.dp, bottomStart = 40.dp)
+                    )
+                )
+                Row(
+                    modifier = Modifier
+                        .padding(top = 48.dp, start = 24.dp, end = 24.dp)
+                        .fillMaxWidth()
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .height(100.dp)
+                            .padding(start = 14.dp)
+                            .weight(0.7f),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.Start
+                    )
+                    {
+                        Text(text ="Hello")
+                    }
+                }
+            }
+        }
+    }
+}
+
